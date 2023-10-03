fc4bca6c8ad2b7298247aa2f8f7933af3033a3ba
Wakeland Branz <105642273+WakelandBranz@users.noreply.github.com>
2023-10-03 16:45:27 -0400

    M5L1 Activity

---
+++ wakelandBranzM5L1Activity/Activity/src/App.java
@@ -0,0 +1,31 @@
+/*
+ * Wakeland Branz
+ * Date: 10/03/2023
+ * Seaforth High School
+ * Practicing operators
+ *
+ * Description:  Takes an input of age and performs math on it based on the age entered.
+ * Difficulties:  I initially forgot to truncate my decimals but I fixed that issue quuickly.
+ * What I Learned:  How to use if statements with Java code.
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 37;
+        int y = 49;
+        int r = 37;
+        boolean b = (x >= y) && !(x==r);
+        boolean c = (x >= y) || !(x==r);
+        // && comes before || in operator precedence
+        boolean d = true && false || false; // operator precedence example
+
+        System.out.printf("B -> %b\nC -> %b\nD -> %b", b, c, d);
+    }
+}
+
+/*
+ * Sample output:
+ * B -> false
+ * C -> false
+ * D -> false
+ */
