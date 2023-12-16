6bce5c6272205a39c1af90bb3093f68251e8f2a8
Wakeland Branz <105642273+WakelandBranz@users.noreply.github.com>
2023-12-15 21:28:08 -0500

    Small changes to newest file

--- wakelandBranzM9L3Assignment/coinFlilp/src/coinRunner.java
+++ wakelandBranzM9L3Assignment/coinFlilp/src/coinRunner.java
@@ -1,22 +1,39 @@
+/*
+ * Wakeland Branz
+ * Date: 12/15/2023
+ * Seaforth High School
+ *
+ * Description:  Creates three coin objects and flips them until they have all landed on the same side
+ * Difficulties:  Figuring out how to check if all coin states were the same took me two tries, but the rest of the assignment was not too difficult.
+ * What I Learned:  How to organize a file with private classes and a runner class
+ */
+
 import java.util.Random;
 
 class Coin {
-    boolean state; // 0 = tails, 1 = heads
+    String state; 
     Random rand = new Random();
 
     public Coin() {}
 
+    // false = tails, true = heads
+    // generate a random boolean and then assign resulting String to this.state
     public void flip() {
-        this.state = this.rand.nextBoolean();
+        boolean res = this.rand.nextBoolean();
+
+        this.state = "tails";
+        if (res) {
+            this.state =  "heads";
+        }
     }
 
     // getters
-    public boolean getState() {
+    public String getState() {
         return this.state;
     }
 
     // setters
-    public void setState(boolean state) {
+    public void setState(String state) {
         this.state = state;
     }
 }
@@ -25,7 +42,7 @@ public class coinRunner {
     public static void main(String[] args) throws Exception {
         int iteration = 0;
 
-        // infinite loop
+        // loop until all coins flip to the same side
         while (true) {
             Coin coin1 = new Coin();
             Coin coin2 = new Coin();
@@ -44,7 +61,7 @@ public class coinRunner {
 
             // if all states are the same
             if (coin1.state == coin2.state && coin1.state == coin3.state) {
-                System.out.println("The coins were flipped a total of " + iteration + " times before landing on the same side.");
+                System.out.println("The coins were flipped a total of " + iteration + " times before landing on the same side (" + coin1.state + ").");
                 return;
             }
 
@@ -53,3 +70,52 @@ public class coinRunner {
         }
     }
 }
+
+/*
+ * Sample output:
+ * Flip #1
+ * Coin 1 -> tails
+ * Coin 2 -> tails
+ * Coin 3 -> heads
+ * 
+ * Flip #2
+ * Coin 1 -> tails
+ * Coin 2 -> heads
+ * Coin 3 -> heads
+ * 
+ * Flip #3
+ * Coin 1 -> heads
+ * Coin 2 -> heads
+ * Coin 3 -> tails
+ * 
+ * Flip #4
+ * Coin 1 -> tails
+ * Coin 2 -> tails
+ * Coin 3 -> heads
+ * 
+ * Flip #5
+ * Coin 1 -> tails
+ * Coin 2 -> heads
+ * Coin 3 -> tails
+ * 
+ * Flip #6
+ * Coin 1 -> tails
+ * Coin 2 -> heads
+ * Coin 3 -> tails
+ * 
+ * Flip #7
+ * Coin 1 -> tails
+ * Coin 2 -> tails
+ * Coin 3 -> heads
+ * 
+ * Flip #8
+ * Coin 1 -> tails
+ * Coin 2 -> heads
+ * Coin 3 -> tails
+ * 
+ * Flip #9
+ * Coin 1 -> tails
+ * Coin 2 -> tails
+ * Coin 3 -> tails
+ * The coins were flipped a total of 9 times before landing on the same side (tails).
+ */
