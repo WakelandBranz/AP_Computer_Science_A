9fb8bbfeb1423e7fad4a4e2d755bb2990819a2a1
Wakeland Branz <105642273+WakelandBranz@users.noreply.github.com>
2024-02-13 19:48:03 -0500

    New AP Classroom assignment and some testing code

--- testEnvironment/testEnv/src/App.java
+++ testEnvironment/testEnv/src/App.java
@@ -7,5 +7,19 @@ public class App {
         return cents / 25;
     }
     
-    
+    //public static int circleTest(ArrayList<Circle> cir) {
+    //    for (int i = 0; i < cir.size(); i++) {
+    //        if (Math.abs(5 - cir[i].getRadius()) < 0.01) {
+    //            return i;
+    //        }
+    //    }
+    //    return -1;
+    //}
 }
+
+
+//class Circle {
+//    public void setRadius(double r);
+//    public double getRadius();
+//    public String toString();
+//}
